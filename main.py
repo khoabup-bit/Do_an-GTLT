0d3b624ee001ea6c89b3c63f901858dcaae41d72
khoabup <khoa09149311@gmail.gmailcom>
2026-01-30 09:46:15 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,92 @@
+# my_name = 'nguyen'
+# MY_NAME = "NGUYEN"
+# print(my_name)
+# print(MY_NAME)
+
+# x = 10
+# y = 20
+# if x == y:
+#     print("x=y")
+# else:
+#     print("x!=y")
+
+# x = "nguyen"
+
+# my_var = 'CNTT' 
+# 0_my_var = 'CNTT'
+# %_my_var = 10
+# def = 10
+# class = "Student"
+
+# def_my_var = 10
+# class_my_var = "Student"
+
+# text = ''
+
+# # Day la comment 1
+# # Day la comment phu
+# 'Day la comment 2'
+# """
+#     Day la comment 3
+#     Day la comment xuong hang
+# """
+
+# ho_ten = 'Nguyen Pham Nhat Khoa'
+# tuoi = 22
+# que_quan = 'Quang Ngai'
+# print("Ho ten:", ho_ten)
+# print("tuoi: ", tuoi)
+# print("que quan:", que_quan)
+
+# number = 1
+# number_second = -2
+# number_zero = 0
+# print(type(number))
+
+# r = 5.5
+# so_pi = 3.14
+# s = so_pi*r**2
+# c = 2*so_pi*r
+# print("Chu vi: ",c)
+# print("Dien tich: ",s)
+
+# name = "Nguyen"
+# name = 'nguyen'
+# print(type(name))   
+
+# is_student = True
+# print(type(is_student))
+# is_student = "abc"
+# print(type(is_student))
+
+# numbers = [1, 2, 3, 4]
+# print(type(numbers))
+# names = ["khoa", "hien", "huyen"]
+# print(type(names))
+
+# datas = []
+
+# my_list = [1, 3, 2, 3, 4, 4]
+# print(my_list)
+# print(type(my_list))
+
+# my_tuple = (1, 2, 4, 4, 5)
+# print(my_tuple)
+# print(type(my_tuple))
+
+# colors = {"red", "red", "blue", "yellow"}
+# numbers = {1, 3, 2, 3, 4, 4}
+# print(colors)
+# print(type(colors))
+
+student = {
+    "name": "Khoa",
+    "age": 22,
+    "class": ["22ct", "python"],
+    "subject": [ 
+    {
+        "score": 10,
+        "is_complete": False
+    }
+]
+}
